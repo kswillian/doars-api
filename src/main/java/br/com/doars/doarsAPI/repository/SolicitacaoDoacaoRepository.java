f81beb18aca122498573323f91b0c4b8468efc70
willian-kaminski <developer.williansantos@gmail.com>
2021-03-31 21:16:16 -0300

    refatoring JPQL interface

--- src/main/java/br/com/doars/doarsAPI/repository/SolicitacaoDoacaoRepository.java
+++ src/main/java/br/com/doars/doarsAPI/repository/SolicitacaoDoacaoRepository.java
@@ -17,21 +17,21 @@ public interface SolicitacaoDoacaoRepository extends JpaRepository<SolicitacaoDo
     Long countSolicitacoesDoacaoAtivas();
 
     @Query(value = "select count(s.*) from solicitacao_doacao s, solicitacao_doacao_tipos_sanguineos sts where " +
-            "s.id = sts.solicitacao_doacao_id and s.ativo = true and s.entidade_id = ?1 and sts.tipo_sanguineos_list_id = ?2", nativeQuery = true)
+            "s.id = sts.solicitacao_doacao_id and s.ativo = true and s.entidade_id = ?1 and sts.tipos_sanguineos_id = ?2", nativeQuery = true)
     Long countSolicitacoesDoacaoAtivasByEntidadeAndTipoSanguineo(Long entidadeId, Long tipoSanguineoDd);
 
     Page<SolicitacaoDoacao> findAllByEntidadeId(Pageable pageable, Long id);
 
-    @Query(value = "select distinct s.* from solicitacao_doacao s, entidade e, solicitacao_doacao_tipo_sanguineos_list sdt, tipo_sanguineo tp \n" +
-            "where s.entidade_id = e.id and s.id = sdt.solicitacao_doacao_id and sdt.tipo_sanguineos_list_id = tp.id and tp.id in ?1 and s.entidade_id = ?2 and s.ativo = true", nativeQuery = true)
+    @Query(value = "select distinct s.* from solicitacao_doacao s, entidade e, solicitacao_doacao_tipos_sanguineos sdt, tipo_sanguineo tp \n" +
+            "where s.entidade_id = e.id and s.id = sdt.solicitacao_doacao_id and sdt.tipos_sanguineos_id = tp.id and tp.id in ?1 and s.entidade_id = ?2 and s.ativo = true", nativeQuery = true)
     Page<SolicitacaoDoacao> findAllByEntidadeIdAndTiposSanguineos(Pageable pageable, List<Long> tiposSanguineosId, Long id);
 
-    @Query(value = "select distinct s.* from solicitacao_doacao s, entidade e, solicitacao_doacao_tipo_sanguineos_list sdt, tipo_sanguineo tp \n" +
-            "where s.entidade_id = e.id and s.id = sdt.solicitacao_doacao_id and sdt.tipo_sanguineos_list_id = tp.id and s.descricao like %?1% and s.entidade_id = ?2 and s.ativo = true", nativeQuery = true)
+    @Query(value = "select distinct s.* from solicitacao_doacao s, entidade e, solicitacao_doacao_tipos_sanguineos sdt, tipo_sanguineo tp \n" +
+            "where s.entidade_id = e.id and s.id = sdt.solicitacao_doacao_id and sdt.tipos_sanguineos_id = tp.id and s.descricao like %?1% and s.entidade_id = ?2 and s.ativo = true", nativeQuery = true)
     Page<SolicitacaoDoacao> findAllByEntidadeIdAndDescricao(Pageable pageable, String search, Long id);
 
-    @Query(value = "select distinct s.* from solicitacao_doacao s, entidade e, solicitacao_doacao_tipo_sanguineos_list sdt, tipo_sanguineo tp \n" +
-            "where s.entidade_id = e.id and s.id = sdt.solicitacao_doacao_id and sdt.tipo_sanguineos_list_id = tp.id and tp.id in ?1 and s.descricao like %?2% and s.entidade_id = ?3 and s.ativo = true", nativeQuery = true)
+    @Query(value = "select distinct s.* from solicitacao_doacao s, entidade e, solicitacao_doacao_tipos_sanguineos sdt, tipo_sanguineo tp \n" +
+            "where s.entidade_id = e.id and s.id = sdt.solicitacao_doacao_id and sdt.tipos_sanguineos_id = tp.id and tp.id in ?1 and s.descricao like %?2% and s.entidade_id = ?3 and s.ativo = true", nativeQuery = true)
     Page<SolicitacaoDoacao> findAllByEntidadeIdAndTiposSanguineosAndDescricao(Pageable pageable, List<Long> tiposSanguineosId, String search, Long id);
 
 }
